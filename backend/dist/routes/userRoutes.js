523d22b469a24f86999b62a44857912e3b3f1fb1
PiyushXmishra <piyushbhp83@gmail.com>
2024-10-24 12:17:49 +0530

    deleting dist folder

--- backend/dist/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const usercontroller_1 = require("../controllers/usercontroller");
-const router = (0, express_1.Router)();
-// Use the route handlers
-router.post('/register', usercontroller_1.registerUser);
-router.post('/login', usercontroller_1.loginUser);
-exports.default = router;
