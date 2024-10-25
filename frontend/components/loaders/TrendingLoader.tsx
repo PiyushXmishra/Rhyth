d03d30c738893540dd49f3389ff715fd84f75d32
PiyushXmishra <piyushbhp83@gmail.com>
2024-10-25 12:32:49 +0530

    Some ui chanegs

--- frontend/components/loaders/TrendingLoader.tsx
+++ frontend/components/loaders/TrendingLoader.tsx
@@ -7,11 +7,11 @@ interface LoadingSkeletonProps {
 
 const LoadingSkeleton: React.FC<LoadingSkeletonProps> = ({ count }) => {
   return (
-    <div className="grid grid-cols-2 gap-4 md:px-2 md:pt-4 w-full">
+    <div className="grid grid-cols-2 gap-4 md:px-2 pt-2 md:pt-4 w-full">
       {Array.from({ length: count }).map((_, index) => (
-        <Skeleton key={index} className="rounded-md md:rounded-xl bg-accent md:p-2 flex flex-row md:flex-col items-center md:space-y-2 cursor-pointers w-full">
+        <Skeleton key={index} className="rounded-md md:rounded-xl bg-accent md:p-2 flex flex-row md:flex-col items-center md:space-y-2 cursor-pointer">
           {/* Image skeleton */}
-          <Skeleton className="relative w-80 h-12 md:h-44 md:w-full overflow-hidden rounded-l-md md:rounded-xl " />
+          <Skeleton className="relative w-80 h-14 md:h-44 md:w-full overflow-hidden rounded-l-md md:rounded-xl " />
           
           {/* Title skeleton */}
           <Skeleton className="pl-2 md:pl-0 font-semibold text-sm md:text-base text-muted-foreground text-center" />
