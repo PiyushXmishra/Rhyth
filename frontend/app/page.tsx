e0394639288e9a1710b1c0d6c9e4b817c942088c
PiyushXmishra <piyushbhp83@gmail.com>
2024-10-25 14:03:31 +0530

    front page changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,8 @@ import UserPlaylists from "@/components/UserPlaylists";
 const Home: React.FC = () => {
   return (
     <>
-    <div className="z-50 flex md:max-h-[calc(100vh-8rem)] md:min-h-[calc(100vh-8rem)] w-full md:bg-secondary rounded-3xl md:pb-4 flex-col ">
+    <div className="z-50 flex md:max-h-[calc(100vh-8rem)] md:min-h-[calc(100vh-8rem)] w-full md:bg-secondary rounded-3xl md:pb-4 pt-2 md:pt-0 flex-col ">
+      <h1 className="flex md:hidden text-2xl p-2 pt-0 font-semibold text-white text-start ">Hey User !</h1>
       <TrendingTracks />
     </div>
     <div className="md:hidden">
