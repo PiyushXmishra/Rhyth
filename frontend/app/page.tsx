70df2b890e2466fea6386acab1c1c98cc1f453c1
PiyushXmishra <piyushbhp83@gmail.com>
2024-10-31 15:59:17 +0530

    Removed username

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,6 @@
-"use client";
-import PlayPauseButton from "@/components/controls/PlayPauseButton";
+"use client"
 import TrendingTracks from "@/components/Trending";
 import UserPlaylists from "@/components/UserPlaylists";
-import { useSession } from "next-auth/react";
 
 const getGreeting = () => {
   const currentHour = new Date().getHours();
@@ -17,16 +15,13 @@ const getGreeting = () => {
 };
 
 const Home: React.FC = () => {
-  
-  const { data: session } = useSession();
-  const userName = session ? session.user?.name?.split(" ")[0] : "User";
   const greeting = getGreeting();
 
   return (
     <>
       <div className="z-50 flex lg:max-h-[calc(100vh-8rem)] lg:min-h-[calc(100vh-8rem)] w-full lg:bg-secondary rounded-3xl lg:pb-4 flex-col ">
         <h1 className="flex lg:hidden text-xl p-2 font-sans font-bold text-white text-start">
-        {greeting}, {userName}!
+        {greeting}
         </h1>
         <TrendingTracks />
       </div>
