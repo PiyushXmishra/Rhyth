e4eb4344821c40d45e77ddac22378fc37ee34422
PiyushXmishra <piyushbhp83@gmail.com>
2025-01-14 18:19:45 +0530

    Adding active states

--- frontend/components/MobileFooter.tsx
+++ frontend/components/MobileFooter.tsx
@@ -1,30 +1,36 @@
+"use client"
 import { House, ListMusic, History, Download } from 'lucide-react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
-import User from './User'
 
 function MobileFooter() {
+  const pathname = usePathname() 
+
+  const getLinkClass = (path:any) => {
+    return pathname === path 
+      ? 'stroke-primary'
+      : 'text-gray-500';  
+  }
+
   return (
-    <div className='flex lg:hidden sticky bottom-0 w-full bg-card mt-auto '>
+    <div className='flex lg:hidden sticky bottom-0 w-full bg-card mt-auto'>
       <div className="flex flex-row justify-between px-8 py-2 items-center w-full">
-        <Link href={"/"}>
+        <Link href={"/"} className={getLinkClass('/')}>
           <House size={28} />
         </Link>
 
-        <Link href={"/yourplaylists"}>
-          <ListMusic size={28}/>
+        <Link href={"/yourplaylists"} className={getLinkClass('/yourplaylists')}>
+          <ListMusic size={28} />
         </Link>
 
-        <Link href={"/history"}>
+        <Link href={"/history"} className={getLinkClass('/history')}>
           <History size={28} />
         </Link>
 
-        <Link href={"/download"}>
+        <Link href={"/download"} className={getLinkClass('/download')}>
           <Download size={28} />
         </Link>
-
-      
-
       </div>
     </div>
   )
--- frontend/components/SideBar.tsx
+++ frontend/components/SideBar.tsx
@@ -1,24 +1,33 @@
 import React from "react";
 import { House, ListMusic, Download, Settings, Heart, History } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
 
 function SideBar() {
+    const pathname = usePathname() 
+  
+    const getLinkClass = (path:any) => {
+      return pathname === path 
+        ? ''
+        : 'text-gray-400';  
+    }
+  
   return (
     <div className="hidden lg:flex bg-secondary  rounded-r-3xl p-2 flex-col justify-between " >
       <div className="flex flex-col space-y-8 p-4">
-        <Link href={"/"}>
+        <Link href={"/"} className={getLinkClass('/')}>
           <House className="w-8 h-8" />
         </Link>
 
-        <Link href={"/yourplaylists"}>
+        <Link href={"/yourplaylists"} className={getLinkClass('/yourplaylists')}>
           <ListMusic className="w-8 h-8" />
         </Link>
 
-        <Link href={"/history"}>
+        <Link href={"/history"} className={getLinkClass('/history')}>
           <History className="w-8 h-8" />
         </Link>
 
-        <Link href={"/download"}>
+        <Link href={"/download"} className={getLinkClass('/download')}>
           <Download className="w-8 h-8" />
         </Link>
       </div>
