1411e51602e7391c370d1fcaa764693668a50cca
PiyushXmishra <piyushbhp83@gmail.com>
2025-01-24 20:30:45 +0530

    Adding history loader

---
+++ frontend/components/loaders/HistoryLoader.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Skeleton } from "@/components/ui/skeleton"; // Import Skeleton component from Shadcn
+
+const HistoryLoader: React.FC<{ count: number }> = ({ count }) => {
+  return (
+    <div>
+      <div className="overflow-y-auto">
+        {[...Array(count)].map((_, index) => (
+          <>
+            <Skeleton className="w-32 h-5 my-2 bg-muted-foreground/30" />
+            <div
+              key={index}
+              className="flex items-center py-2 lg:my-2 bg-transparent"
+            >
+              <Skeleton className="w-20 h-12 lg:w-28 lg:h-16 bg-muted-foreground/30" />
+              <div className="ml-4 w-full space-y-2">
+                <Skeleton className="h-4 w-full bg-muted-foreground/30" />
+                <Skeleton className="h-4 w-44 bg-muted-foreground/30 " />
+              </div>
+            </div>
+            <Skeleton
+              key={index}
+              className="flex items-center py-2 lg:my-2 bg-transparent"
+            >
+              <Skeleton className="w-20 h-12 lg:w-28 lg:h-16 bg-muted-foreground/30" />
+              <div className="ml-4 w-full space-y-2">
+                <Skeleton className="h-4 w-full bg-muted-foreground/30" />
+                <Skeleton className="h-4 w-44 bg-muted-foreground/30 " />
+              </div>
+            </Skeleton>
+            <Skeleton
+              key={index}
+              className="flex items-center py-2 lg:my-2 bg-transparent"
+            >
+              <Skeleton className="w-20 h-12 lg:w-28 lg:h-16 bg-muted-foreground/30" />
+              <div className="ml-4 w-full space-y-2">
+                <Skeleton className="h-4 w-full bg-muted-foreground/30" />
+                <Skeleton className="h-4 w-44 bg-muted-foreground/30 " />
+              </div>
+            </Skeleton>
+            <Skeleton
+              key={index}
+              className="flex items-center py-2 lg:my-2 bg-transparent"
+            >
+              <Skeleton className="w-20 h-12 lg:w-28 lg:h-16 bg-muted-foreground/30" />
+              <div className="ml-4 w-full space-y-2">
+                <Skeleton className="h-4 w-full bg-muted-foreground/30" />
+                <Skeleton className="h-4 w-44 bg-muted-foreground/30 " />
+              </div>
+            </Skeleton>
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HistoryLoader;
