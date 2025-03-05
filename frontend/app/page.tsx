65ea174bd0d973e1f315584e2207d4bd1aada29c
PiyushXmishra <piyushbhp83@gmail.com>
2025-03-05 19:07:28 +0530

    Updating track ids

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,15 +1,9 @@
-"use client"
-
 import Greeting from "@/components/Greeting";
 import RecentlyPlayed from "@/components/RecentlyPlayed";
 import TrendingTracks from "@/components/Trending";
-import { useGradient } from "@/components/contexts/GradientContext";
 const Home: React.FC = () => {
-
-  const { gradient } = useGradient();
   return (
     <>
-   
     <div className="relative flex h-full w-full lg:pb-4 flex-col overflow-y-auto">
         <Greeting/>  
         <TrendingTracks />
--- frontend/components/contexts/TracksContext.tsx
+++ frontend/components/contexts/TracksContext.tsx
@@ -1,11 +1,10 @@
-// src/context/TracksContext.tsx
 "use client"
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
 interface Track {
-    id: string;   // Track ID
-    name: string; // Track name
+    id: string;  
+    name: string; 
     thumbnails: {
       default?: { url: string; width: number; height: number };
       medium?: { url: string; width: number; height: number };
@@ -15,48 +14,44 @@ interface Track {
     };
   }
 
-// Define the type for the context
 interface TracksContextType {
-  tracks: Track[]; // State to hold track data
-  loading: boolean; // Loading state
+  tracks: Track[]; 
+  loading: boolean; 
 }
 
-// Create a context
 const TracksContext = createContext<TracksContextType | undefined>(undefined);
 
-// Define track IDs outside of the component
 const trackIds = [
   "RDCLAK5uy_n9Fbdw7e6ap-98_A-8JYBmPv64v-Uaq1g",
-  "RDEMj0weLcfSL_fmHFYGkc7UEQ",
-  "PL9bw4S5ePsEG_nRuruWzLzfXeqNIWOtHe",
   "RDCLAK5uy_kuo_NioExeUmw07dFf8BzQ64DFFTlgE7Q",
-  "PLO7-VO1D0_6M1xUjj8HxTxskouWx48SNw",
-  "RDCLAK5uy_lj-zBExVYl7YN_NxXboDIh4A-wKGfgzNY"
+  "RDCLAK5uy_lj-zBExVYl7YN_NxXboDIh4A-wKGfgzNY",
+  "RDCLAK5uy_n17q7_2dwfDqWckpccDyTTkZ-g03jXuII",
+  "RDCLAK5uy_mOvRWCE7v4C98UgkSVh5FTlD3osGjolas", 
+  "RDCLAK5uy_mUvTtdERIHEiVAHIkV3GRndrY-H4M2nnA"
 ];
 
 export const TracksProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [tracks, setTracks] = useState<Track[]>([]); // State to hold track data
-  const [loading, setLoading] = useState<boolean>(true); // State to track loading status
+  const [tracks, setTracks] = useState<Track[]>([]); 
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchTrendingTracks = async () => {
       try {
-        // Fetch only if tracks are empty
         if (tracks.length === 0) {
           const response = await axios.post<Track[]>(`${process.env.NEXT_PUBLIC_URL}/api/playlist/homeplaylists`, {
             playlistIds: trackIds
           });
-          setTracks(response.data); // Set the tracks
+          setTracks(response.data); 
         }
       } catch (error) {
         console.error('Failed to fetch tracks:', error);
       } finally {
-        setLoading(false); // Ensure loading is set to false after the API call
+        setLoading(false); 
       }
     };
 
     fetchTrendingTracks();
-  }, [tracks.length]); // Run only if tracks length is 0
+  }, [tracks.length]); 
 
   return (
     <TracksContext.Provider value={{ tracks, loading }}>
@@ -65,7 +60,6 @@ export const TracksProvider: React.FC<{ children: React.ReactNode }> = ({ childr
   );
 };
 
-// Custom hook to use TracksContext
 export const useTracks = () => {
   const context = useContext(TracksContext);
   if (!context) {
